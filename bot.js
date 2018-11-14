89dedaf724e7bc9cafbf130d817eca8e495592e5
justbegingfuckeerzz <44809221+justbegingfuckeerzz@users.noreply.github.com>
2018-11-14 13:58:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,26 +12,22 @@ client.on('message', message => {
          if(message.guild.id !=="495233573264293915") return 
 
             client.user.setStatus('online');
-        console.log(Edit Status to: ['online'] by ${message.author.tag} (${message.author.id}));
      }
 
      if(message.content.startsWith(prefix + 'setS idle')) {
          if(message.guild.id !=="495233573264293915") return 
             client.user.setStatus('idle');
-        console.log(Edit Status to: ['idle'] by ${message.author.tag} (${message.author.id}));
      }
 
 
      if(message.content.startsWith(prefix + 'setS offline')) {
          if(message.guild.id !=="495233573264293915") return 
             client.user.setStatus('invisible');
-        console.log(Edit Status to: ['invisible'] by ${message.author.tag} (${message.author.id}));
      }
 
      if(message.content.startsWith(prefix + 'setS dnd')) {
          if(message.guild.id !=="495233573264293915") return 
             client.user.setStatus('dnd');
-        console.log(Edit Status to: ['dnd'] by ${message.author.tag} (${message.author.id}));
      }
 });
 
