d8b0dbb4a0bc8954035d2ff12ffc7bf0ba536005
justbegingfuckeerzz <44809221+justbegingfuckeerzz@users.noreply.github.com>
2018-11-14 13:57:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = '!!';
 client.on('ready' , () => { 
-    console.log("${client.user.tag} ready...");
     console.log('Fukcing ');
 });
 
